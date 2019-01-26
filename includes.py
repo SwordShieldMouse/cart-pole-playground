b790de186fdf701f2d9754b933d5f5d712078ac8
SwordShieldMouse <alke8882000@gmail.com>
2019-01-25 23:55:57 -0700

    added basic stats for policy gradient training

--- includes.py
+++ includes.py
@@ -1,6 +1,7 @@
 import gym
 import math
 import random
+import time
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
--- main.py
+++ main.py
@@ -7,14 +7,15 @@ env = gym.make('CartPole-v0').unwrapped
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 policy = Policy(4, 2).to(device)
 
-optimizer = optim.Adam(policy.parameters(), lr = 1e-3)
+optimizer = optim.Adam(policy.parameters(), lr = 1e-2)
 
-episodes = 100
-gamma = 0.8
+episodes = 1000
+gamma = 0.9
 
 for episode in range(episodes):
     obs = env.reset()
     rewards = []
+    times = []
     for t in range(1000):
         env.render()
         obs = torch.FloatTensor(obs).to(device)
@@ -28,7 +29,8 @@ for episode in range(episodes):
         rewards.append(reward)
 
         if done:
-            print("Episode done after {} timesteps".format(t))
+            #print("Episode done after {} timesteps".format(t))
+            times.append(t)
             break
     # do update after the episode
     T = len(rewards)
@@ -36,10 +38,17 @@ for episode in range(episodes):
     gammas = [1]
     for i in range(1, T):
         gammas.append(gamma * rewards[i - 1])
+
     gammas = Variable(torch.FloatTensor(gammas).to(device), requires_grad = True)
+
+    # the loss function is just the negative of the value function
     loss = -torch.sum(torch.mul(rewards, gammas))
+
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
 
-    # the loss function is just the negative of the value function
+    if episode % 100 == 0:
+        print("On episode {}".format(episode + 1))
+        print("Avg length of episode for last 100 episodes is {}".format(np.mean(times)))
+        times = []
