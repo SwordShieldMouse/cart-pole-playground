a1ea383eafd48db649df4f69c48ff0a715185e2d
SwordShieldMouse <alke8882000@gmail.com>
2019-01-26 18:48:17 -0700

    fixed rewards again

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ policy = Policy(4, 2).to(device)
 optimizer = optim.Adam(policy.parameters(), lr = 1e-2)
 
 episodes = 1000
-gamma = 0.9
+gamma = 0.99
 
 for episode in range(episodes):
     obs = env.reset()
     rewards = []
     times = []
+    log_probs = Variable(torch.FloatTensor([]).to(device), requires_grad = True)
     for t in range(1000):
         env.render()
         obs = torch.FloatTensor(obs).to(device)
@@ -23,10 +24,11 @@ for episode in range(episodes):
         # take an action
         logits = policy(obs)
         c = torch.distributions.Categorical(logits = logits)
-        action = c.sample().item()
+        action = c.sample()
+        log_probs = torch.cat([log_probs, torch.unsqueeze(c.log_prob(action), 0)])
 
         # get the next state and reward
-        obs, reward, done, info = env.step(action)
+        obs, reward, done, info = env.step(action.item())
 
         rewards.append(reward)
 
@@ -37,22 +39,33 @@ for episode in range(episodes):
     # do update after the episode
     T = len(rewards)
 
-    gammas = [gamma]
-    for i in range(1, T):
-        gammas.append(gamma * gammas[i - 1])
+    for ix in range(T):
+        rewards[ix] *= gamma ** ix
+
+    returns = []
+    #gammas = [gamma ** i for i in range(T)]
+
+    R = 0
+    for r in rewards[::-1]:
+        R = r + gamma * R
+        returns.insert(0, R)
 
     # let autograd see the variables
-    rewards = Variable(torch.FloatTensor(rewards).to(device), requires_grad = True)
-    gammas = Variable(torch.FloatTensor(gammas).to(device), requires_grad = True)
+    #rewards = Variable(torch.FloatTensor(rewards).to(device), requires_grad = True)
+    #gammas = Variable(torch.FloatTensor(gammas).to(device), requires_grad = True)
+    returns = torch.FloatTensor(returns).to(device)
 
     # the loss function is just the negative of the value function
-    loss = -torch.sum(torch.mul(rewards, gammas))
+    #loss = -torch.sum(torch.mul(torch.mul(rewards, gammas), log_probs))
+    loss = -torch.sum(torch.mul(returns, log_probs))
 
     optimizer.zero_grad()
     loss.backward()
+    #torch.autograd.gradcheck()
+    #print(loss.grad)
     optimizer.step()
 
-    if episode % 100 == 0:
+    if episode % 100 == 0 and episode > 0:
         print("On episode {}".format(episode + 1))
         print("Avg length of episode for last 100 episodes is {}".format(np.mean(times)))
         times = []
